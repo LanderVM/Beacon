a32d8123b16ed2a4995f982411ae6096aa273b1f
LanderVM <lander.vanmolle@student.hogent.be>
2024-05-23 16:19:16 +0200

    Disable beacon connectable option

--- ibeacon/ibeacon.ino
+++ ibeacon/ibeacon.ino
@@ -21,76 +21,12 @@
 
 // Generated using: https://www.uuidgenerator.net/version4
 #define DEVICE_NAME         "Blue Pulse Dev"
-#define SERVICE_UUID        "fbe88b60-93a8-4366-9271-11f2ba389c2a"
-#define BEACON_UUID         "9b3addbd-4f00-4a38-8f17-5f3bab4b8650"
 #define BEACON_UUID_REV     "e485ef7e-a0c8-4e8b-9c9b-7d8db9007a32"
-#define CHARACTERISTIC_UUID "7a03ba71-1029-4dc4-8dda-f7f502d4f7ee"
-
 
 #define MAJOR 5
 #define MINOR 80
 
 BLEServer *pServer;
-BLECharacteristic *pCharacteristic;
-bool deviceConnected = false;
-uint8_t value = 0;
-
-class MyServerCallbacks : public BLEServerCallbacks {
-  void onConnect(BLEServer *pServer) {
-    deviceConnected = true;
-    Serial.println("deviceConnected = true");
-  };
-
-  void onDisconnect(BLEServer *pServer) {
-    deviceConnected = false;
-    Serial.println("deviceConnected = false");
-
-    // Restart advertising to be visible and connectable again
-    BLEAdvertising *pAdvertising;
-    pAdvertising = pServer->getAdvertising();
-    pAdvertising->start();
-    Serial.println("iBeacon advertising restarted");
-  }
-};
-
-class MyCallbacks : public BLECharacteristicCallbacks {
-  void onWrite(BLECharacteristic *pCharacteristic) {
-    String rxValue = pCharacteristic->getValue();
-
-    if (rxValue.length() > 0) {
-      Serial.println("*********");
-      Serial.print("Received Value: ");
-      for (int i = 0; i < rxValue.length(); i++) {
-        Serial.print(rxValue[i]);
-      }
-      Serial.println();
-      Serial.println("*********");
-    }
-  }
-};
-
-void init_service() {
-  BLEAdvertising *pAdvertising;
-  pAdvertising = pServer->getAdvertising();
-  pAdvertising->stop();
-
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(BLEUUID(SERVICE_UUID));
-
-  // Create a BLE Characteristic
-  pCharacteristic = pService->createCharacteristic(
-    CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY
-  );
-  pCharacteristic->setCallbacks(new MyCallbacks());
-  pCharacteristic->addDescriptor(new BLE2902());
-
-  pAdvertising->addServiceUUID(BLEUUID(SERVICE_UUID));
-
-  // Start the service
-  pService->start();
-
-  pAdvertising->start();
-}
 
 void init_beacon() {
   BLEAdvertising *pAdvertising;
@@ -102,7 +38,7 @@ void init_beacon() {
   myBeacon.setManufacturerId(0x4c00);
   myBeacon.setMajor(MAJOR);
   myBeacon.setMinor(MINOR);
-  myBeacon.setSignalPower(0xc5);
+  myBeacon.setSignalPower(0xCD); //-51 Dbw for 1m range from beacon
   myBeacon.setProximityUUID(BLEUUID(BEACON_UUID_REV));
 
   BLEAdvertisementData advertisementData;
@@ -110,6 +46,7 @@ void init_beacon() {
   advertisementData.setManufacturerData(myBeacon.getData());
   pAdvertising->setAdvertisementData(advertisementData);
 
+  pAdvertising->setAdvertisementType(ADV_TYPE_NONCONN_IND); // Non-connectable undirected advertising
   pAdvertising->start();
 }
 
@@ -120,23 +57,15 @@ void setup() {
   Serial.flush();
 
   BLEDevice::init(DEVICE_NAME);
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
+  BLEDevice::setPower(ESP_PWR_LVL_P20); // Set transmit power to +20 dBm
 
-  init_service();
+  pServer = BLEDevice::createServer();
+
   init_beacon();
 
   Serial.println("iBeacon + service defined and advertising!");
 }
 
 void loop() {
-  if (deviceConnected) {
-    Serial.printf("*** NOTIFY: %d ***\n", value);
-    pCharacteristic->setValue(&value, 1);
-    pCharacteristic->notify();
-    pCharacteristic->setValue("Dit is een custom service");
-
-    value++;
-  }
-  delay(2000);
+  // No need for loop content in beacon-only mode
 }
